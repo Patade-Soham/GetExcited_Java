c837dbe9e943129e1fcf48dd16a7e5c452f4a099
Soham Patade <sohampatade30@eng.rizvi.edu.in>
2025-08-06 23:49:10 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -3,22 +3,20 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the word to check if it is palindrome or not : ");
-        String word=sc.nextLine();
+        System.out.print("Enter the word to check if it is palindrome or not: ");
+        String word = sc.nextLine();
         String rev = "";
-        for(int i = word.length() - 1;i>=0;i--){
+
+        for(int i = word.length() - 1; i >= 0; i--) {
             rev = rev + word.charAt(i);
-            System.err.println(rev);
-
-        }
-        if(word==rev){
-            System.out.println(word+ " is a Palindrome. ");
-        }
-        else
-        {
-            System.out.println(word+" is not a Palindrome.");
         }
 
-        
+        if(word.equals(rev)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is not a Palindrome.");
+        }
+
+        sc.close(); 
     }
 }
